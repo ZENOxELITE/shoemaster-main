469320cfa61c56bbcbf700bc310a3c00d5db2665
Muneer Ahmed <156942752+ZENOxELITE@users.noreply.github.com>
2025-08-15 02:18:14 +0500

    Update db.ts

--- dist/server/db.ts
+++ dist/server/db.ts
@@ -2,13 +2,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env" });
+dotenv.config();
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
-const db = drizzle(pool);
-
-export { pool, db }; // named exports
+export const db = drizzle(pool);
+export { pool };
